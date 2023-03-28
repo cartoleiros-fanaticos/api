8ca971470d28a22c3288bb2fdef6bbaa3391eb7e
Wedson Santos <wedson_cross@hotmail.com>
2023-03-28 06:41:02 -0300

    Trabalhando na tela analise do atleta

--- app/Http/Controllers/AtletasController.php
+++ app/Http/Controllers/AtletasController.php
@@ -8,6 +8,7 @@ use App\Models\Posicoes;
 use App\Models\Scouts;
 use App\Models\Status;
 use Illuminate\Http\Request;
+use DB;
 
 class AtletasController extends Controller
 {
@@ -56,22 +57,108 @@ class AtletasController extends Controller
 
     public function show(Request $request, string $id)
     {
-        $atleta = Atletas::select('atleta_id', 'apelido', 'foto', 'variacao_num', 'preco_num', 'pontos_num', 'media_num', 'jogos_num', 'clube_id', 'posicao_id', 'status_id', 'rodada_id')
-            ->selectRaw('(SELECT CONCAT(clube_casa_id, \'x\',  clube_visitante_id) FROM partidas WHERE rodada = rodada_id AND (clube_casa_id = clube_id OR clube_visitante_id = clube_id)) AS confronto')
-            ->selectRaw('(SELECT DATE_FORMAT(partida_data, "%d/%m %H:%i") FROM partidas WHERE rodada = rodada_id AND (clube_casa_id = clube_id OR clube_visitante_id = clube_id)) AS partida_data')
-            ->where('atleta_id', $id)
-            ->first();
 
-        $clubes = Clubes::select('id', 'nome', 'abreviacao', '60x60')
-            ->get()
-            ->keyBy('id');
+        $atleta = DB::SELECT('
+            SELECT 
+                atleta_id,
+                apelido,
+                foto,
+                variacao_num,
+                preco_num,
+                pontos_num,
+                media_num,
+                jogos_num,
+                clube_id,
+                posicoes.nome as posicao_nome,
+                posicao_id,
+                status_id,
+                rodada_id,
+                G,
+                A,
+                DS,
+                FD,
+                FF,
+                FT,
+                FS,
+                GS,
+                SG,
+                DP,
+                GC,
+                CA,
+                CV,
+                FC,
+                I,
+                PP,
+                PS,
+                PC,
+                DE,
 
-        $posicoes = Posicoes::select('id', 'nome')
-            ->get()
-            ->keyBy('id');
+                clube_casa_id,
+                clube_visitante_id,
+                DATE_FORMAT(partida_data, "%d/%m %H:%i") partida_data
 
-        $status = Status::select('id', 'nome')
-            ->get()
+            FROM atletas
+            INNER JOIN partidas ON rodada = rodada_id AND (clube_casa_id = clube_id OR clube_visitante_id = clube_id)
+            INNER JOIN posicoes ON posicoes.id = posicao_id
+        ')[0];
+
+        $geral = DB::select('
+            SELECT 
+                IFNULL(SUM(pontuacao), 0) pontuacao,
+                IFNULL(AVG(pontuacao), 0) media,
+                COUNT(partidas.id) jogos
+            FROM parciais 
+            INNER JOIN partidas ON partidas.rodada = parciais.rodada AND (clube_casa_id = clube_id OR clube_visitante_id = clube_id)
+            WHERE atleta_id = ?
+        ', [$atleta->atleta_id])[0];
+
+        $casa = DB::select('
+            SELECT 
+                IFNULL(SUM(pontuacao), 0) pontuacao,
+                IFNULL(AVG(pontuacao), 0) media,
+                COUNT(partidas.id) jogos
+            FROM parciais
+            INNER JOIN partidas ON partidas.rodada = parciais.rodada AND clube_casa_id = clube_id
+            WHERE atleta_id = ?
+        ', [$atleta->atleta_id])[0];
+
+        $fora = DB::select('
+            SELECT 
+                IFNULL(SUM(pontuacao), 0) pontuacao,
+                IFNULL(AVG(pontuacao), 0) media,
+                COUNT(partidas.id) jogos
+            FROM parciais
+            INNER JOIN partidas ON partidas.rodada = parciais.rodada AND clube_visitante_id = clube_id
+            WHERE atleta_id = ?
+        ', [$atleta->atleta_id])[0];
+
+        $atleta->pontuacao = [
+            'pontuacao_geral' => $geral->pontuacao,
+            'pontos_casa' => $casa->pontuacao,
+            'pontos_fora' => $fora->pontuacao,
+        ];
+
+        $atleta->media = [
+            'media_geral' => $geral->media,
+            'media_casa' => $casa->media,
+            'media_fora' => $fora->media,
+        ];
+
+        $atleta->jogos = [
+            'jogos_geral' => $geral->jogos,
+            'jogos_casa' => $casa->jogos,
+            'jogos_fora' => $fora->jogos,
+        ];
+
+        $clubes = COLLECT(DB::SELECT('
+            SELECT 
+                id,
+                nome,
+                abreviacao,
+                60x60 as escudo
+            FROM clubes
+            WHERE id IN (?, ?)
+        ', [$atleta->clube_casa_id, $atleta->clube_visitante_id]))
             ->keyBy('id');
 
         $scouts = Scouts::select('sigla', 'nome')
@@ -80,8 +167,6 @@ class AtletasController extends Controller
         return response()->json([
             'atleta' => $atleta,
             'clubes' => $clubes,
-            'posicoes' => $posicoes,
-            'status' => $status,
             'scouts' => $scouts,
         ]);
     }
---
+++ database/migrations/2023_03_28_082520_create_parciais_table.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('parciais', function (Blueprint $table) {
+
+            $table->id();            
+
+            $table->integer('rodada');
+
+            $table->foreignId('clube_id')->constrained('clubes');
+            $table->foreignId('atleta_id')->constrained('atletas');
+            
+            $table->float('pontuacao');
+            $table->float('valorizacao')->nullable();
+
+            $table->integer('DS')->default(0);
+            $table->integer('FC')->default(0);
+            $table->integer('GC')->default(0);
+            $table->integer('CA')->default(0);
+            $table->integer('CV')->default(0);
+            $table->integer('SG')->default(0);
+            $table->integer('DP')->default(0);
+            $table->integer('GS')->default(0);
+
+            $table->integer('FS')->default(0);
+            $table->integer('A')->default(0);
+            $table->integer('FT')->default(0);
+            $table->integer('FD')->default(0);
+            $table->integer('FF')->default(0);
+            $table->integer('G')->default(0);
+            $table->integer('I')->default(0);
+            $table->integer('PP')->default(0);
+
+            $table->integer('PS')->default(0);
+            $table->integer('PC')->default(0);
+            $table->integer('DE')->default(0);
+
+            $table->unique([ 'rodada', 'atleta_id' ]);
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('parciais');
+    }
+};
