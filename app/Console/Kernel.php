1b04799432b72b00414cfebeb6f9a15a9966ef4b
Wedson Santos <wedson_cross@hotmail.com>
2023-04-07 17:49:16 -0300

    Ajuste cron parciais

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
 
+        $schedule->command('app:parciais')
+            ->everyMinute();
+
         $schedule->command('app:game')
             ->everyTenMinutes();
 
